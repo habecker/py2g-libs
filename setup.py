971a007ffbb364579c1d519943ab910ea7bd07c3
Yannick Habecker <y@y2g.de>
2020-10-16 14:48:52 +0200

    add setup script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Py2g-utilities',
+      version='0.0-1',
+      description='Some python utilities',
+      author='Yannick Habecker',
+      author_email='yannick@py.y2g.de',
+      url='https://www.python.org/sigs/distutils-sig/',
+      packages=['py2g_utils'],
+     )
